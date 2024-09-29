f69d6013e38a7005846749a1a97db33c41b3ea1f
Alf <sh4mil.nasyrov@gmail.com>
2024-09-29 18:03:25 +0500

    replace request logic from user create

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,7 +1,7 @@
 import allure
 import pytest
-import requests
-from data import TestData
+from tests.shared.request import Requests
+from tests.shared.data import TestData
 
 
 
@@ -9,13 +9,14 @@ class TestCreateUser():
     @allure.title('Проверка регистрации нового пользователя')
     @allure.description('При регистрации нового пользователя, вернутся пользовательские данные и код 200')
     def test_create_unique_user_return_success(self, credentials):
-
-        payload = {
+        test_request = Requests(
+            TestData.CREATE_USER_URL,
+            {
             "email": credentials["email"],
             "password": credentials['password'],
             "name": credentials['name']
-        }
-        response = requests.post(TestData.CREATE_USER, data=payload, timeout=10)
+            })
+        response = test_request.post()
         assert response.status_code == 200 and \
                response.json()["success"] == True and \
                response.json()["user"] == {
@@ -27,14 +28,15 @@ class TestCreateUser():
     @allure.title('Проверка регистрации уже существующего пользователя')
     @allure.description('При регистрации уже существующего пользователя, вернётся ошибка и код 403')
     def test_create_user_that_already_exist_return_403(self, credentials):
-
-        payload = {
+        test_request = Requests(
+            TestData.CREATE_USER_URL,
+            {
             "email": credentials["email"],
             "password": credentials['password'],
             "name": credentials['name']
-        }
-        response = requests.post(TestData.CREATE_USER, data=payload, timeout=10)
-        response = requests.post(TestData.CREATE_USER, data=payload, timeout=10)
+            })
+        test_request.post()
+        response = test_request.post()
         assert response.status_code == 403 and \
                response.json() == {
                    "success": False,
@@ -45,8 +47,11 @@ class TestCreateUser():
     @allure.description('При регистрации нового пользователя без обязательного поля, вернётся ошибка и код 403')
     @pytest.mark.parametrize("payload", TestData.REQUIRED_REGISTRATION_FIELD_SUITES)           
     def test_create_user_without_required_field_return_403(self, payload):
-
-        response = requests.post(TestData.CREATE_USER, data=payload, timeout=10)
+        test_request = Requests(
+            TestData.CREATE_USER_URL,
+            payload
+            )
+        response = test_request.post()
         assert response.status_code == 403 and \
                response.json() == {
                    "success": False,
